da62c72c2e6b337008a9753d835289c859b5b39e
James Whiteley IV <jameswhiteleyiv@gmail.com>
2021-06-24 16:57:23 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -2,18 +2,35 @@ import sys
 import os
 import shutil
 
-
 if sys.platform == 'win32':
     home = os.environ['USERPROFILE']
 else:
     home = os.environ['HOME']
-colors_dir = os.path.join(home, 'vimfiles', 'colors')
-
-if not os.path.exists(colors_dir):
-    os.makedirs(colors_dir)
-
-shutil.copyfile('gitconfig', os.path.join(home, '.gitconfig'))
-shutil.copyfile('vimrc', os.path.join(home, '.vimrc'))
-shutil.copytree('colors', colors_dir)
 
 
+def copy_colors():
+    colorsdir = os.path.join(home, 'vimfiles', 'colors')
+    if not os.path.exists(colorsdir):
+        os.makedirs(colorsdir)
+
+    srcdir = os.path.abspath('colors')
+    for item in os.listdir(srcdir):
+        src = os.path.join(srcdir, item)
+        dst = os.path.join(colorsdir, item)
+        if not os.path.exists(dst):
+            shutil.copy(src, dst)
+
+
+def copy_dotfiles():
+    gitconfig = os.path.join(home, '.gitconfig')
+    if not os.path.exists(gitconfig):
+        shutil.copyfile('gitconfig', gitconfig)
+
+    vimrc = os.path.join(home, '.vimrc')
+    if not os.path.exists(vimrc):
+        shutil.copyfile('vimrc', vimrc)
+
+
+if __name__ == "__main__":
+    copy_colors()
+    copy_dotfiles()
